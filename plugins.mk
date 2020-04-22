3a91fb837ba0e8bfa3a4cb898881650d22fca7b9
Loïc Hoguin <essen@ninenines.eu>
2020-04-22 13:26:42 +0200

    Fix Make warning on Windows

--- plugins.mk
+++ plugins.mk
@@ -4,7 +4,7 @@ SOURCES += $(THIS)nif_helpers.c
 $(C_SRC_OUTPUT_FILE): $(THIS)nif_helpers.o
 
 ifeq ($(PLATFORM),msys2)
-	CFLAGS += -I"$(THIS)/compat/"
+CFLAGS += -I"$(THIS)/compat/"
 endif
 
 CFLAGS += -I"$(THIS)"
